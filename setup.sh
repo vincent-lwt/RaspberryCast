96e248499d11857c05ab6e30db8f7c9374ab1e71
Rudy Nurhadi <rudynurhadi@gmail.com>
2020-04-16 11:38:42 +0700

    setup: Add omxplayer-wrapper package

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ echo "Installing necessary dependencies... (This could take a while)"
 echo ""
 echo "============================================================"
 
-apt-get install -y lsof python-pip git wget omxplayer libnss-mdns fbi
+apt-get install -y lsof python-pip git wget omxplayer libnss-mdns fbi libglib2.0-dev libdbus-1-dev
 echo "============================================================"
 
 if [ "$?" = "1" ]
@@ -48,7 +48,7 @@ then
   exit 0
 fi
 
-pip install youtube-dl bottle livestreamer
+pip install youtube-dl bottle livestreamer omxplayer-wrapper
 
 if [ "$?" = "1" ]
 then
